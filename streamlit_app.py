fce0946885d0830c2ea921267d55785d21edf396
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-01-26 21:56:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,17 @@ import os
 # Autenticación de OpenAI (oculta la clave en una variable de entorno)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+# Funcion para leer los criterios
+def read_criterios():
+    with open("criterios.txt", "r") as f:
+        criterios = f.read()
+    return criterios
 
-# Funcion para generar una respuesta a partir de un prompt
+# Funcion para generar la respuesta
 def generate_response(prompt):
+    
+    
+    # Generar la respuesta
     completions = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
@@ -18,26 +26,22 @@ def generate_response(prompt):
     )
 
     message = completions.choices[0].text
-    return message
+    return message.strip()
 
-# Leer los criterios del archivo externo
-def read_criterios():
-    with open("criterios.txt", "r") as f:
-        criterios = f.read()
-        f.close()
-    return criterios
+def main():
+    # Titulo de la aplicacion
+    st.title("App de Resolucion de Casos de Etica")
+    
+    # Pedir al usuario que ingrese el caso a resolver
+    case_study = st.text_area("Ingrese el caso a resolver:")
 
-# Titulo de la aplicacion
-st.title("App de Resolucion de Casos de Etica")
+    # Generar la respuesta
+    if st.button("Resolver caso"):
+        # Leer los criterios
+        criterios = read_criterios()
+        prompt = criterios + case_study
+        response = generate_response(prompt)
+        st.success("La respuesta es: \n" + response)
 
-# Leer los criterios
-criterios = read_criterios()
-
-# Pedir al usuario que ingrese el caso a resolver
-case_study = st.text_area("Ingrese el caso a resolver:")
-
-# Generar la respuesta
-if st.button("Resolver caso"):
-    prompt = criterios + case_study
-    response = generate_response(prompt)
-    st.success("La respuesta es: \n" + response)
+if __name__ == '__main__':
+    main()
